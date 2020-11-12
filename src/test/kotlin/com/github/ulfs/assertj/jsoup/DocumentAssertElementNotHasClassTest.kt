4a5ceab8f360ed14eb2aba2f938dcf9c4eed8d88
Ulf Sauer <ulf.sauer@otto.de>
2020-11-13 00:08:15 +0100

    Test elementNotHasClass

---
+++ src/test/kotlin/com/github/ulfs/assertj/jsoup/DocumentAssertElementNotHasClassTest.kt
@@ -0,0 +1,93 @@
+package com.github.ulfs.assertj.jsoup
+
+import com.github.ulfs.assertj.jsoup.Assertions.Companion.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.assertj.core.util.FailureMessages.actualIsNull
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import kotlin.test.Test
+
+class DocumentAssertElementNotHasClassTest {
+
+    @Test
+    fun `should fail if element is null`() {
+        // given
+        val nullDocument: Document? = null
+
+        // when / then
+        assertThatThrownBy {
+            assertThat(nullDocument).elementNotHasClass(".class", "content")
+        }
+            .isInstanceOf(AssertionError::class.java)
+            .hasMessage(actualIsNull())
+    }
+
+    @Test
+    fun `should fail if element does not exist`() {
+        // given
+        val document: Document = Jsoup.parse("")
+
+        // when / then
+        assertThatThrownBy {
+            assertThat(document).elementNotHasClass(".class", "content")
+        }
+            .isInstanceOf(AssertionError::class.java)
+            .hasMessage(
+                """
+                
+                Expecting element for
+                  <.class>
+                but found nothing
+                """.trimIndent()
+            )
+    }
+
+    @Test
+    fun `should fail if element has class`() {
+        // given
+        val document: Document = Jsoup.parse("""<div class="class content">text</div>""")
+
+        // when
+        assertThatThrownBy {
+            assertThat(document).elementNotHasClass(".class", "content")
+        }
+            .isInstanceOf(AssertionError::class.java)
+            .hasMessage(
+                """
+                        
+                        Expecting element for
+                          <.class>
+                        to not include class
+                          <content>
+                        but was
+                          <<div class="class content">
+                         text
+                        </div>>
+                        """.trimIndent()
+            )
+
+        // then
+        // no exception is thrown
+    }
+
+    @Test
+    fun `should pass if only inner element has class`() {
+        // given
+        val document: Document = Jsoup.parse("""<div class="class"><span class="content">text</span></div>""")
+
+        // when
+        assertThat(document).elementNotHasClass(".class", "content")
+
+        // then
+        // no exception is thrown
+    }
+
+    @Test
+    fun `should pass if element does not have class`() {
+        // given
+        val document: Document = Jsoup.parse("""<div class="class cont">text</div>""")
+
+        // when / then
+        assertThat(document).elementNotHasClass(".class", "content")
+    }
+}
