ab0a1605526e49037582fa35eb808ce50d9d9fa5
Ulf Sauer <ulf.sauer@otto.de>
2021-07-25 20:54:54 +0200

    Create PGP key in root dir

--- ci/load-gpg-key.sh
+++ ci/load-gpg-key.sh
@@ -11,9 +11,9 @@ if [ -f "$ENV_FILE" ] ;then
 fi
 
 # base64-encoded armored GPG key
-GPG_KEY_FILE="release.asc"
+GPG_KEY_FILE="../release.asc"
 # intermediary GPG key
-GPG_KEY_BIN_FILE="release.gpg"
+GPG_KEY_BIN_FILE="../release.gpg"
 
 echo "$GPG_SIGNING_KEY" | base64 --decode > "$GPG_KEY_FILE"
 gpg --quiet --output "$GPG_KEY_BIN_FILE" --dearmor "$GPG_KEY_FILE"
