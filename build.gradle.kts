cf5a573b1c128c93888c84012674a90b8855b05d
Ulf Sauer <ulf.sauer@otto.de>
2021-07-26 16:45:06 +0200

    Bump assertj-core from 3.18.1 to 3.20.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("org.assertj:assertj-core:3.18.1")
+    implementation("org.assertj:assertj-core:3.20.2")
     implementation("org.jsoup:jsoup:1.14.1")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
